9b58ac2fed0db29ccc3ccecaec9b6306c1bf69eb
abpwrs <alexander-powers@uiowa.edu>
2019-11-15 13:51:33 -0600

    TEST: testing sphinx docs gen

--- setup.py
+++ setup.py
@@ -18,5 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+          'itk','pandas','numpy',
+      ],
     python_requires=">=3.6",
 )
